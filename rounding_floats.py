bdbcf7b549a3777a50fbe5535ca64a687a651e76
wrekone <wrekone@users.noreply.github.com>
2016-02-17 13:41:48 -0800

    Update rounding_floats.py

--- rounding_floats.py
+++ rounding_floats.py
@@ -1,6 +1,7 @@
-float_num = 7.7
+float_num = 7.73549
 
 int(float_num)    # round down
 
 int(float_num) + 1    # round up
 
+round(float_num, 2)    # round to number of digits round(number[, ndigits])
